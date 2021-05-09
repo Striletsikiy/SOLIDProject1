dfaba874e0a3939168cb56303f5bb0d2edf7bbf2
Arlamovskiy-Ruslan <55766007+Arlamovskiy-Ruslan@users.noreply.github.com>
2021-05-09 15:55:26 +0300

    SOLID

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,19 @@
+import io.Data;
+import service.ServiceSum;
+
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Data data = new Data(scanner);
+        ServiceSum service = new ServiceSum();
 
+        int a = data.input("Enter a: ");
+        int b = data.input("Enter b: ");
+
+        int sum = service.calculate(a, b);
+        data.print("Result: ");
+        data.print(sum);
     }
 }
